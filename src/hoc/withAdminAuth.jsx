09ddc9a18b1dd144818caec8da1e03977bf186aa
Sujal Raval <88503112+s123u@users.noreply.github.com>
2025-07-26 00:17:24 +0530

    admin login 14

--- src/hoc/withAdminAuth.jsx
+++ src/hoc/withAdminAuth.jsx
@@ -77,7 +77,7 @@ export default function withAdminAuth(Component, allowedRoles = []) {
                 } finally {
                     setIsLoading(false);
                 }
-            };ch
+            };
 
             verifyAuth();
         }, [navigate, allowedRoles]);
